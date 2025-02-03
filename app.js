78f457dfa90013c2d6744c90774816a974aced90
ROBERTA MEYRELLES FRANÇA <roberta.franca@uvvnet.com.br>
2025-02-03 19:19:10 -0300

    adição da funcionalidade: adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []; //variável para armazenas os nomes dos amigos que forem sendo adicionados através do input
+
+//Tarefa 1 
+//Implementar uma função para adicionar amigos
+function adicionarAmigo() {
+  //capturo o elemento Dom 
+  let inputAmigo = document.getElementById('amigo');
+  //capturo agora o valor do campo
+  let nomeAmigo = inputAmigo.value;
+  console.log(nomeAmigo);
+
+  //validar a entrada para garantir que o campo não esteja vazio 
+  if (!nomeAmigo) {
+    alert("Por favor, insira um nome.");
+  }
+  //adiciono o nome do amigo ao array
+  amigos.push(nomeAmigo);
+
+  //limpo o campo de input
+  inputAmigo.value = '';
+  console.log(amigos);
+}
+
